88ea1f9e1210255cfd7a7977ab46ffec6554c941
Luke Fleuri <lukefleuri@gmail.com>
2019-07-10 11:31:56 -0500

    Initial push

---
+++ CustomListProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListProjectTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8bbdc40c-9db3-46e2-8d95-34b12e230dfc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListProjectTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+
+namespace CustomListProjectTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void Add_AddToEmptyList_ValueGoesToIndexZero()
+        {
+            //arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //act
+            test.Add(1);
+            actual = test[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void Add_AddToList_CountIncrementsProperly()
+        {
+            //arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //act
+            test.Add(1);
+            actual = test.ListCount; //?Create a custom property that accounts for how many items are in my custom list
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_AddToListWithTwoValues_NewValueGoesToIndexTwo()
+        {
+            //arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            //act
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            actual = test[2];
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
